a9733c44d2de6b882ee5f55504f7a051e84d4399
wisnia97 <1b13wisniewski@gmail.com>
2020-06-12 11:19:30 +0200

    Add random lines from one point

--- random.js
+++ random.js
@@ -19,7 +19,7 @@ function randomPoints(pktRandom){
     for(i = 0; i < pktRandom; i++){
         x = getRandomInt(0, 500);
         y = getRandomInt(0, 500);
-        DrawPoint(x, y, 256);
+        DrawPoint(x, y, 0);
     }
   }
 
@@ -34,6 +34,19 @@ function randomPoints(pktRandom){
         y1 = getRandomInt(0, 500);
         x2 = getRandomInt(0, 500);
         y2 = getRandomInt(0, 500);
-        DrawLine(x1, y1, x2, y2, 09000)
+        DrawLine(x1, y1, x2, y2, 0)
+    }
+  }
+
+  function randomDirectionLine(lin_x, lin_y, linRandom){
+    console.log(linRandom);
+    console.log(lin_x);
+    console.log(lin_y);
+    var x2 = 0;
+    var y2
+    for(i = 0; i < linRandom; i++){
+        x2 = getRandomInt(0, 500);
+        y2 = getRandomInt(0, 500);
+        DrawLine(lin_x, lin_y, x2, y2, 0)
     }
   }
